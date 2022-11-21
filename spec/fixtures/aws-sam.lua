cccf90b8f97e0655861436a24aad3ed0caa1825d
windmgc <windmgc@gmail.com>
2022-11-21 16:52:28 +0800

    fix

--- spec/fixtures/aws-sam.lua
+++ spec/fixtures/aws-sam.lua
@@ -37,7 +37,12 @@ function _M.start_local_lambda()
     return nil, "No available port found"
   end
 
-  local ret, err = utils.wait_output("sam local start-lambda --template-file=spec/fixtures/sam-app/template.yaml --port " .. port, "Starting the Local Lambda Service", 180)
+  -- run in background
+  local t = ngx.thread.spawn(function()
+    utils.execute("sam local start-lambda --template-file=spec/fixtures/sam-app/template.yaml --port " .. port)
+  end)
+
+  local ret, err = utils.execute("pgrep sam")
   if err then
     return nil, fmt("Start SAM CLI failed(code: %s): %s", err, ret)
   end
