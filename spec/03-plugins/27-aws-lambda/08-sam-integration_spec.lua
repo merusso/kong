1d902272981d3182da47dafff877a0581e986d39
windmgc <windmgc@gmail.com>
2022-11-22 15:02:29 +0800

    test

--- spec/03-plugins/27-aws-lambda/08-sam-integration_spec.lua
+++ spec/03-plugins/27-aws-lambda/08-sam-integration_spec.lua
@@ -77,6 +77,11 @@ if sam.get_os_architecture() ~= "aarch64" then
 
           local ret, err = utils.execute("curl http://localhost:" .. sam_port .. "/2015-03-31/functions/HelloWorldFunction/invocations -d '{}'")
           print(inspect(ret), inspect(err))
+
+          assert(helpers.pwait_until(function()
+            local ret, err = utils.wait_output("curl http://localhost:" .. sam_port .. "/2015-03-31/functions/HelloWorldFunction/invocations -d '{}'")
+            assert.is_nil(err)
+          end, 120))
         end)
 
         it("invoke a simple function", function ()
